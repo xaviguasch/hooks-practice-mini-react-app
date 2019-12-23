c1f5750ffbb6797aa135407f587af060db571ad7
xaviguasch <xaviguasch@gmail.com>
2019-12-23 16:52:38 +0100

    set another hook practice miniapp

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,10 @@
-import React, { useState } from 'react'
-import './App.css'
+import React from 'react'
 
 function App() {
-    const [count, setCount] = React.useState(0)
-
-    const increase = () => {
-        setCount(count + 1)
-    }
-    const decrease = () => {
-        setCount(count - 1)
-    }
-
     return (
-        <div className='App'>
-            <div className='container'>
-                <h1>{count}</h1>
-                <button onClick={increase}>+</button>
-                <button onClick={decrease}>-</button>
-            </div>
+        <div className='container'>
+            <h1>TIME</h1>
+            <button>Get Time</button>
         </div>
     )
 }
